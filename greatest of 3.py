d6ed50a5917e10fe230b9c5bcccbebb87f9dc482
vidya-yc <117058427+vidya-yc@users.noreply.github.com>
2022-11-04 20:46:47 +0530

    Add files via upload

---
+++ greatest of 3.py
@@ -0,0 +1,11 @@
+# Write your code here :-)
+num1=10
+num2=15
+num3=20
+if(num1>=num2)and(num1>=num3):
+    greatest=num1
+elif(num2>=num1)and(num2>=num3):
+    greatest=num2
+else:
+    greatest=num3
+print("the greatest number is",greatest)
