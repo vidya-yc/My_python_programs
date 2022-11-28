bb4c39208bcf7cabad622cbc972e04ad42aa6e89
vidya-yc <117058427+vidya-yc@users.noreply.github.com>
2022-11-28 22:07:33 +0530

    Add files via upload

---
+++ removing.py
@@ -0,0 +1,9 @@
+y=[]
+l=int(input("enter the length of the list"))
+for i in range(l):
+    a = int(input("Enter a values of list :"))
+    y.append(a)
+print(y)
+print(" remove all the occurances of a number entered by the user ")
+y.clear()
+print(y)
