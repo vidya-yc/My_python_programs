fde171b1b21a60d6bc07ad44aeb8a75ac16e3c11
vidya-yc <117058427+vidya-yc@users.noreply.github.com>
2022-11-28 18:36:49 +0530

    Add files via upload

---
+++ average %.py
@@ -0,0 +1,7 @@
+sub1=int(input("Enter marks of the first subject: "))
+sub2=int(input("Enter marks of the second subject: "))
+sub3=int(input("Enter marks of the third subject: "))
+sub4=int(input("Enter marks of the fourth subject: "))
+sub5=int(input("Enter marks of the fifth subject: "))
+avg=(sub1+sub2+sub3+sub4+sub4)/5
+print(avg)
