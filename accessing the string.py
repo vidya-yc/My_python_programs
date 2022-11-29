85fdc0a91dd5c592257f93075616590526cf2e32
vidya-yc <117058427+vidya-yc@users.noreply.github.com>
2022-11-29 18:37:49 +0530

    Add files via upload

---
+++ accessing the string.py
@@ -0,0 +1,14 @@
+str = "HELLO!"
+print(str[0])
+print(str[2])
+print(str[4])
+#negative indexing
+str = "HOW ARE YOU !"
+print(str[-1])
+print(str[-5])
+print(str[12])
+#slicing
+str ="HOWS LIFE !"
+print(str[: 3])
+print(str[1 : 5 : 2])
+print(str[-1 : -12 : -2])
