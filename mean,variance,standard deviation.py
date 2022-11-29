28ed31e81f73b11f7fc3a297d61cfceaaa8d6dfb
vidya-yc <117058427+vidya-yc@users.noreply.github.com>
2022-11-29 18:40:46 +0530

    Add files via upload

---
+++ mean,variance,standard deviation.py
@@ -0,0 +1,15 @@
+import statistics
+a = list()
+number = int(input( "Enter the number of elements you want: "))
+print ('Enter numbers in array: ')
+for i in range(int(number)):
+    n = input( "number : ")
+    a.append ( int ( n ))
+print("Given 'n' numbers: ")
+print(a)
+m = statistics.mean ( a )
+v = statistics.variance ( a )
+st = statistics.stdev ( a )
+print( "Mean: ",m)
+print( "Variance: ",v)
+print( "Standard Deviation: ",st)
